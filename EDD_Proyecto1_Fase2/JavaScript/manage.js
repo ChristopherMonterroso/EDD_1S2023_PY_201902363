c8149864af3f3848bb24779220469aecfd424d06
ChristopherMonterroso <chrismonc08@gmail.com>
2023-03-28 22:29:35 -0600

    update

---
+++ EDD_Proyecto1_Fase2/JavaScript/manage.js
@@ -0,0 +1,12 @@
+function authenticateUser(){
+    var user = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (user =="admin" && password =="admin") {
+        openAdminPage()
+    }else{
+        alert("no funciona")
+    }
+}
+function openAdminPage(){
+    window.location.replace("/html/admin.html")
+}
