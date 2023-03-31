c524f2e83f56032fa5b041b84a4196e2932629fa
ChristopherMonterroso <chrismonc08@gmail.com>
2023-03-31 00:38:03 -0600

    upload admin responsive page

--- EDD_Proyecto1_Fase2/html/navbarAdmin.js
+++ EDD_Proyecto1_Fase2/html/navbarAdmin.js
@@ -1,9 +1,17 @@
+
+function logOut() {
+	window.location.replace("index.html")
+}
+//Código para desplegar el botón cuando la pantalla sea pequeña
+
+//función para activar una pestaña al ser activadas
 $('ul.tabs li a:first').addClass('active');
 $('.section article').hide();
 $('.section article:first').show();
 
 $('ul.tabs li a').on('click', function () {
 	$('ul.tabs li a').removeClass('active');
+	$('.nav-bar').removeClass('active')
 	$(this).addClass('active')
 	$('.section article').hide()
 	var activeTab = $(this).attr('href');
@@ -11,7 +19,3 @@ $('ul.tabs li a').on('click', function () {
 	$(activeTab).show()
 	return false;
 });
-
-function logOut(){
-	window.location.replace("index.html")
-}
